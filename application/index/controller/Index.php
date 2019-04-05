fc2375e28faa8f64bcf920d3f8d0881463bc3bb0
LuckyHJH <luckyhjh@qq.com>
2019-04-06 01:17:39 +0800

    init

---
+++ application/index/controller/Index.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\index\controller;
+
+use app\common\controller\Frontend;
+
+class Index extends Frontend
+{
+
+    protected $noNeedLogin = '*';
+    protected $noNeedRight = '*';
+    protected $layout = '';
+
+    public function _initialize()
+    {
+        parent::_initialize();
+    }
+
+    public function index()
+    {
+        exit;
+    }
+
+}
