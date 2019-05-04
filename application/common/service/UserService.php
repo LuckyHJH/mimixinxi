73466258272ad97f5c635cbde4bbc7c410eceff2
LuckyHJH <luckyhjh@qq.com>
2019-05-04 22:52:53 +0800

    下载头像到本地

--- application/common/service/UserService.php
+++ application/common/service/UserService.php
@@ -34,19 +34,19 @@ class UserService extends Model
      * @param $userInfo
      * @param string $session_key
      * @return array|bool
-     * @throws \think\Exception
-     * @throws \think\exception\DbException
+     * @throws \Exception
      */
     public function updateUserInfo($openid, $userInfo, $session_key = '')
     {
         if (empty($openid) || !isset($userInfo['nickName']) || !isset($userInfo['avatarUrl'])) {
             return false;
         }
+        $avatar = $this->downloadAvatar($userInfo['avatarUrl'], $openid);
 
         $row = UserModel::get(['openid'=>$openid]);
         if ($row) {
             $row->nickname = $userInfo['nickName'];
-            $row->avatar = $userInfo['avatarUrl'];
+            $row->avatar = $avatar;
             $row->weixin_info = json_encode($userInfo);
             $row->update_time = time();
             $session_key and $row->session_key = $session_key;
@@ -56,7 +56,7 @@ class UserService extends Model
             $row = [
                 'openid' => $openid,
                 'nickname' => $userInfo['nickName'],
-                'avatar' => $userInfo['avatarUrl'],
+                'avatar' => $avatar,
                 'weixin_info' => json_encode($userInfo),
                 'create_time' => time(),
             ];
@@ -66,4 +66,29 @@ class UserService extends Model
         return $result;
     }
 
+    /**
+     * 下载头像到本地
+     * @param $url
+     * @param $openid
+     * @return bool|string 成功时返回头像路径
+     */
+    public function downloadAvatar($url, $openid)
+    {
+        $data = file_get_contents($url);
+        if (empty($data) || empty($openid)) {
+            return false;
+        }
+
+        $fileName = md5($openid);
+        $path = substr($fileName, 0, 2);
+        $uploadDir = "/uploads/avatar/{$path}";
+        $path = ROOT_PATH . '/public' . $uploadDir;
+        $avatar = "{$uploadDir}/{$fileName}";
+        if (!(is_dir($path) || mkdir($path, 0755, true))) {
+            return false;
+        }
+
+        $res = file_put_contents("{$path}/{$fileName}", $data);
+        return $res ? $avatar : $url;
+    }
 }
